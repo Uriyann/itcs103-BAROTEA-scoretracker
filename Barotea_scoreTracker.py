40f6d9bf0735bc2aadb29b843e5145556237b290
Uriyann <joshuabarotea06@gmail.com>
2025-05-02 22:26:21 +0800

    fixed and finished the quiz #2, ready to be delivered

--- Barotea_scoreTracker.py
+++ Barotea_scoreTracker.py
@@ -36,6 +36,7 @@ def save_info_to_excel():
         ws = wb.active
         ws.title = "Userdata"
         ws.append(["Name", "Score", "Remarks"])
+        ws.append(["", "", ""])
 
     if score >= 75 and score <= 100:
         ws.append([name, score, "Passed"])
@@ -53,9 +54,6 @@ def save_info_to_excel():
     username_entry.delete(0, END)
     score_entry.delete(0, END)
 
-    username_entry.insert(0, "Name")
-    score_entry.insert(0, "Score")
-
 # Update Function
 def update_info_to_excel():
     if not user_data_debugger():
@@ -64,50 +62,60 @@ def update_info_to_excel():
     name = username_entry.get().strip().lower()
     score = int(score_entry.get())
 
-    wb = load_workbook("student_scores.xlsx")
-    ws = wb["Userdata"]
+    try:
+        wb = load_workbook("student_scores.xlsx")
+        ws = wb["Userdata"]
 
-    for row in ws.iter_rows(min_row=2):
-        cell_name = row[0].value
-        if cell_name and cell_name.strip().lower() == name:
-            row[1].value = score
-            if score >= 75 and score <= 100:
-                row[2].value = "Passed"
-            elif score >= 50 and score < 75:
-                row[2].value = "Failed"
-            else:
-                row[2].value = "You Need To Study More"
-            wb.save("student_scores.xlsx")
+        for row in ws.iter_rows(min_row=2):
+            cell_name = row[0].value
+            if cell_name and cell_name.strip().lower() == name:
+                row[1].value = score
+                if score >= 75 and score <= 100:
+                    row[2].value = "Passed"
+                elif score >= 50 and score < 75:
+                    row[2].value = "Failed"
+                else:
+                    row[2].value = "You Need To Study More"
+                wb.save("student_scores.xlsx")
 
-            messagebox.showinfo(title="Success", message="Score Updated Successfully.")
-            return True
+                messagebox.showinfo(title="Success", message="Score Updated Successfully.")
+                return True
 
-    messagebox.showerror(title="Error", message="User not found.")
-    return False
+        messagebox.showerror(title="Error", message="User not found.")
+        return False
 
+    except Exception as e:
+        messagebox.showerror(title="Error", message=str(e))
+        return False
 
 # Show Data Function
 def show_data_excel():
-    wb = load_workbook("student_scores.xlsx")
-    ws = wb["Userdata"]
+    try:
 
-    for row in ws.iter_rows(min_row=2, values_only=True):
-        print(row)
+        wb = load_workbook("student_scores.xlsx")
+        ws = wb["Userdata"]
 
-# Formula Fixer Function
-def formula_excel():
-    if not user_data_debugger():
-        return
-    
-    wb = load_workbook("student_scores.xlsx")
-    ws = wb["Userdata"]
+        data_window = CTkToplevel(window)
+        data_window.title("Data")
+        data_window.geometry("350x400")
+        data_window.resizable(False, False)
+        data_window.configure(fg_color="#2b2b2b")
 
-    for row in ws.iter_rows(min_row=2):
-        ws.cell(row=row[0].row, column=2, value=f'=AVERAGE(B{row[0].row}:B{row[0].row+1})')
+        toplevel_frame = CTkFrame(data_window, fg_color="#2b2b2b")
+        toplevel_frame.pack(pady=10, padx=10, fill="both", expand=True)
 
+        for i, row in enumerate(ws.iter_rows(values_only=True)):
+            for j, value in enumerate(row):
+                label = CTkLabel(toplevel_frame, text=value, padx=6, pady=3, font=("Arial", 16), fg_color="#2b2b2b")
+                label.grid(row=i, column=j)
 
-    wb.save("student_scores.xlsx")
-    print("awefwf")
+        destroy_button = CTkButton(data_window, text="Close", command=data_window.destroy, font=("Arial", 16))
+        destroy_button.pack(side="bottom", pady=10)
+
+        
+
+    except Exception as e:
+        messagebox.showerror(title="Error", message=str(e))
 
 # Format Fixer Function
 def format_excel():
@@ -154,6 +162,7 @@ def validate_name_str():
         return True
     else:
         messagebox.showerror("Invalid Input", "Please enter a valid name (letters only).")
+        username_entry.delete(0, END)
         return False
     
 # Score Debugger
@@ -165,35 +174,19 @@ def validate_score_int():
         
         else:
             messagebox.showerror(title="Error", message="Please enter a valid score.")
+            score_entry.delete(0, END)
             return False
         
     except ValueError:
         messagebox.showerror(title="Error", message="Please enter a real number.")
+        score_entry.delete(0, END)
         return False
 
 # ================= ENTRY EVENTS ================= #
-# Username Entry Events
-def username_on_click(event):
-    if username_entry.get() == "Name":
-        username_entry.delete(0, END)
-def username_on_leave(event):
-    if username_entry.get() == "":
-        username_entry.insert(0, "Name")
-
-# Score Entry Events
-def score_on_click(event):
-    if score_entry.get() == "Score":
-        score_entry.delete(0, END)
-def score_on_leave(event):
-    if score_entry.get() == "":
-        score_entry.insert(0, "Score")
-
 # User Handle Enter Event
 def user_handle_enter(event):
     if not save_info_to_excel():
         return
-    
-
 
 # ================= MAIN FRAMES ================= #
 main_frame = CTkFrame(window, border_width= 3, corner_radius= 15)
@@ -202,7 +195,12 @@ main_frame.place(rely=0.5, relx=0.5, anchor=CENTER)
 background_image = Image.open(r".\588ee4b393c082b8422e7307e0ece441.png")
 bg_img = CTkImage(light_image=background_image, dark_image=background_image, size=(90, 90))
 bg_label = CTkLabel(main_frame, image=bg_img, text="")
-bg_label.grid(row=0, column=0, sticky="n", pady= 15, padx= 15)
+bg_label.grid(row=0, column=0, sticky="nw", pady= 15, padx= 55)
+
+background_image = Image.open(r".\e4140a754afb75b19463aefe149e65ab.png")
+bg_img = CTkImage(light_image=background_image, dark_image=background_image, size=(90, 90))
+bg_label = CTkLabel(main_frame, image=bg_img, text="")
+bg_label.grid(row=0, column=0, sticky="ne", pady= 15, padx= 55)
 
 # ================= WIDGETS ================= #
 title_label = CTkLabel(main_frame, text= "User Score Tracker", font= ("Times New Roman bold", 35))
@@ -213,8 +211,6 @@ username_label.grid(row=2, column=0, padx=10, pady=5, sticky="w")
 
 username_entry = CTkEntry(main_frame, font=("Arial", 18), border_width=0, placeholder_text="Name", height= 10, width= 10, fg_color= "transparent", bg_color= "transparent")
 username_entry.grid(row=3, column=0, sticky="nwe", padx=10)
-username_entry.bind("<FocusIn>", username_on_click)
-username_entry.bind("<FocusOut>", username_on_leave)
 username_entry.bind("<Return>", user_handle_enter)
 
 user_line = CTkFrame(main_frame, width=300, height=3)
@@ -225,20 +221,18 @@ score_label.grid(row=5, column=0, sticky="w", padx=10, pady=5)
 
 score_entry = CTkEntry(main_frame, font=("Arial", 18), border_width=0, placeholder_text="Score", height= 10, width= 10, fg_color= "transparent", bg_color= "transparent")
 score_entry.grid(row=6, column=0, sticky="nwe", padx=10)
-score_entry.bind("<FocusIn>", score_on_click)
-score_entry.bind("<FocusOut>", score_on_leave)
 score_entry.bind("<Return>", user_handle_enter)
 
 score_line = CTkFrame(main_frame, width=300, height=3)
 score_line.grid(row=7, column=0, sticky= S)
 
-save_button = CTkButton(main_frame, text="Save", font=("Arial", 15), width=13, command=save_info_to_excel)
+save_button = CTkButton(main_frame, text="Save", font=("Arial", 15), width=13, height=30, command=save_info_to_excel)
 save_button.grid(row=8, column=0, padx=20, pady=20, sticky="w")
 
-update_button = CTkButton(main_frame, text="Update", font=("Arial", 15), width=13, command=update_info_to_excel)
+update_button = CTkButton(main_frame, text="Update", font=("Arial", 15), width=13, height=30, command=update_info_to_excel)
 update_button.grid(row=8, column=0, padx=20, pady=20, sticky="e")
 
-show_data = CTkButton(main_frame, text="Show Data", font=("Arial", 15), width=13, command=show_data_excel)
+show_data = CTkButton(main_frame, text="Show Data", font=("Arial", 15), width=13, height=40, command=show_data_excel)
 show_data.grid(row=8, column=0, padx=20, pady=20)
 
 # ================= WINDOW SCORE TRACKER STARTER ================= #
